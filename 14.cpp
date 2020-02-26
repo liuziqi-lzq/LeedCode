645c83836713d1adc0681ea489b4decb4e2ce7c2
liuziqi-lzq <61456843+liuziqi-lzq@users.noreply.github.com>
2020-02-26 18:17:56 +0800

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,11 @@
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *p = head, *q = head;
+    if (p == NULL) return false;
+    do {
+        p = p->next;
+        q = q->next;
+        if (q == NULL || q->next == NULL) return false; 
+        q = q->next;
+    } while (p != q);
+    return true;
+}
